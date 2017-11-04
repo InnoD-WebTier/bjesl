d70510f9636ac12557b8eb6d58244ab9072ee9c6
C Park <changyeonpark.97@gmail.com>
2017-11-03 20:44:41 -0700

    Officer stuff

--- src/components/OfficerPortrait.jsx
+++ src/components/OfficerPortrait.jsx
@@ -3,9 +3,12 @@ import '../styles/OfficerPortrait.scss';
 
 const OfficerPortrait = (props) => {
   return (
-    <div className='OfficerPortrait'>
-      <img alt='fuck linters' src={props.img}></img>
-      <div>{props.name}</div>
+    <div className='OfficerPortrait__Container'>
+      <img alt='' className='OfficerPortrait__Thumbnail' src={props.img}></img>
+      <div className='OfficerPortrait__Biography'>
+        <h4>{props.name}</h4>
+        <p>{props.bio}</p>
+      </div>
     </div>
   );
 };
--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,38 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
 
+import Navbar from '../components/Navbar';
 import './index.css';
-
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
-);
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -43,7 +14,7 @@ const TemplateWrapper = ({ children }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header />
+    <Navbar pages={['about', 'blog', 'events', 'publications', 'contact']} />
     <div
       style={{
         margin: '0 auto',
