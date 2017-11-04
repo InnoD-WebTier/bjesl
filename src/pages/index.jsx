99fae6e18753553ada92e253e057620073e5b89f
C Park <changyeonpark.97@gmail.com>
2017-11-03 20:21:59 -0700

    events into blog page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'gatsby-link';
-import Event from '../components/Event';
+import OfficerPortrait from '../components/OfficerPortrait';
 
 const IndexPage = () => (
   <div>
@@ -8,28 +8,56 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
 
-    <h3> Events </h3>
-    <Event
-      title="SELS Speaker Series: Oakland Raiders GC "
-      date="OCT 11"
-      info="Come see Dan Ventrelle, the General Counsel for the Oakland Raiders!
-      Presentation topics may include Raiders licensing, compliance with NFL league rules,
-      franchise relocation, and much more."
-    />
-
-    <Event
-      title="3rd Annual Sports Industry Conference"
-      date="APR 13"
-      info="Berkeley Law Sports & Law Conference 2017 will take place April 13-14th.
-      Stay tuned for more details!"
-    />
-
-    <Event
-      title="SELS Speaker Series: PIXAR"
-      date="FEB 22, 2017"
-      info="Please join SELS as we welcome Jim Kennedy who is the Chief
-      Legal Counsel at PIXAR Animation Studios."
-    />
+    <div className="OfficerPortraits">
+      <OfficerPortrait
+        name="Jane Doe"
+        bio="Jane Doe is a 3L at UC Berkeley School of Law and is one of the Editors-in-Chief
+             of the Berkeley Journal of Entertainment and Sports Law."
+        img="http://via.placeholder.com/150x150"
+      />
+      <OfficerPortrait
+        name="Jane Doe"
+        bio="Jane Doe is a 3L at UC Berkeley School of Law and is one of the Editors-in-Chief
+             of the Berkeley Journal of Entertainment and Sports Law."
+        img="http://via.placeholder.com/150x150"
+      />
+      <OfficerPortrait
+        name="Jane Doe"
+        bio="Jane Doe is a 3L at UC Berkeley School of Law and is one of the Editors-in-Chief
+             of the Berkeley Journal of Entertainment and Sports Law."
+        img="http://via.placeholder.com/150x150"
+      />
+      <OfficerPortrait
+        name="Jane Doe"
+        bio="Jane Doe is a 3L at UC Berkeley School of Law and is one of the Editors-in-Chief
+             of the Berkeley Journal of Entertainment and Sports Law."
+        img="http://via.placeholder.com/150x150"
+      />
+      <OfficerPortrait
+        name="Jane Doe"
+        bio="Jane Doe is a 3L at UC Berkeley School of Law and is one of the Editors-in-Chief
+             of the Berkeley Journal of Entertainment and Sports Law."
+        img="http://via.placeholder.com/150x150"
+      />
+      <OfficerPortrait
+        name="Jane Doe"
+        bio="Jane Doe is a 3L at UC Berkeley School of Law and is one of the Editors-in-Chief
+             of the Berkeley Journal of Entertainment and Sports Law."
+        img="http://via.placeholder.com/150x150"
+      />
+      <OfficerPortrait
+        name="Jane Doe"
+        bio="Jane Doe is a 3L at UC Berkeley School of Law and is one of the Editors-in-Chief
+             of the Berkeley Journal of Entertainment and Sports Law."
+        img="http://via.placeholder.com/150x150"
+      />
+      <OfficerPortrait
+        name="Jane Doe"
+        bio="Jane Doe is a 3L at UC Berkeley School of Law and is one of the Editors-in-Chief
+             of the Berkeley Journal of Entertainment and Sports Law."
+        img="http://via.placeholder.com/150x150"
+      />
+    </div>
     <Link to="/page-2/">Go to page 2</Link>
 
   </div>
