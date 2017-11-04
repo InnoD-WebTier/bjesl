768f381aa1c8d53667eed45c6a65d324bc8e6588
Ethan Lee <hello@ethanl.ee>
2017-11-03 17:01:19 -0700

    Navbar with routing

---
+++ src/components/NavbarLink.jsx
@@ -0,0 +1,19 @@
+/* global window: false */
+import React from "react";
+import Link from 'gatsby-link';
+
+const NavbarLink = (props) => {
+  const selected = window.location.href.indexOf(props.location) > -1;
+  const label = props.location[0].toUpperCase() + props.location.substring(1);
+
+  return (<div className={`navbar__col${selected ? ' navbar__col--selected' : ''}`}>
+    <Link
+      className={`navbar__col_link${selected ? ' navbar__col_link--selected' : ''}`}
+      to={`/${props.location}`}
+    >
+      {label}
+    </Link>
+  </div>);
+};
+
+export default NavbarLink;
